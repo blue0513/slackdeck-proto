4236bd2c129a10bf6aed639a4b0f434706a60f90
Taiju Aoki <aokitaiju0513@gmail.com>
2019-10-27 20:17:55 +0900

    first commit

--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@ const store = new Store();
 // global variables
 const json = loadSettings();
 const menuModule = require('./menu');
+const Library = require('./lib/lib');
 
 const defaultChannel = 'unreads';
 let uniqueIndex = 0;
@@ -31,7 +32,7 @@ function initialize() {
   });
 
   // create webviews in div
-  const webviews = getWebviews();
+  const webviews = Library.getWebviews();
   webviews.forEach((webview, index) => {
     webview.addEventListener('dom-ready', () => {
       initializeWebview(webview, contents[index].channel_id);
@@ -63,7 +64,7 @@ function generateMenuItemForWorkspaces() {
   const menuItem = new MenuItem(
     { id: 'workspaces', label: 'Workspaces', submenu: [] },
   );
-  const nameAndUrls = getOtherWorkspacesInfo(json.other_urls);
+  const nameAndUrls = Library.getOtherWorkspacesInfo(json.other_urls);
   const otherWorkspacesMenuItems = generateOtherWorkspaceMenuItems(nameAndUrls);
 
   otherWorkspacesMenuItems.forEach((owsMenuItem) => {
@@ -99,70 +100,34 @@ function generateOtherWorkspaceMenuItems(nameAndUrls) {
 
   return otherWorkspacesMenuItems;
 }
-function getOtherWorkspacesInfo(otherUrls) {
-  const nameAndUrls = otherUrls.map((url) => {
-    const workspaceName = new URL(url).hostname.replace(/.slack.com/g, '');
-    return { name: workspaceName, url: new URL(url) };
-  });
-  return nameAndUrls;
-}
-function getWebviews() {
-  return Array.from(document.getElementsByTagName('webview'));
-}
-function getNumberOfWebviews() {
-  return getWebviews().length;
-}
 function initializeWebview(webview, channelId) {
   registerToOpenUrl(webview);
-  const resizableWebview = setWebviewAutosize(webview, 'on');
+  const resizableWebview = Library.setWebviewAutosize(webview, 'on');
 
-  if (checkUrlIsDefault(resizableWebview)) {
-    const channelUrl = getChannelUrl(json.url, channelId);
+  if (Library.checkUrlIsDefault(resizableWebview)) {
+    const channelUrl = Library.getChannelUrl(json.url, channelId);
     loadURL(resizableWebview, channelUrl);
   }
 
-  const onlyBodyCss = getOnlyBodyCss();
-  const onlyChannelCss = getOnlyChannelCss();
-  const onlySidebarCss = getOnlySidebarCss();
+  const onlyBodyCss = Library.getOnlyBodyCss();
+  const onlyChannelCss = Library.getOnlyChannelCss();
+  const onlySidebarCss = Library.getOnlySidebarCss();
   selectAplicableCss(resizableWebview, { onlyBodyCss, onlyChannelCss, onlySidebarCss });
 }
 // TODO: integrate with `initializeWebview`
 function initializeWebviewForAnotherWorkspace(webview, workspaceUrl) {
   registerToOpenUrl(webview);
-  const resizableWebview = setWebviewAutosize(webview, 'on');
+  const resizableWebview = Library.setWebviewAutosize(webview, 'on');
 
-  if (checkUrlIsDefault(resizableWebview)) {
+  if (Library.checkUrlIsDefault(resizableWebview)) {
     const channelId = defaultChannel;
-    const url = getChannelUrl(workspaceUrl, channelId);
+    const url = Library.getChannelUrl(workspaceUrl, channelId);
     loadURL(resizableWebview, url);
   }
 }
-function getOnlySidebarCss() {
-  const disableChannelList = '.p-workspace__sidebar { display: none !important; }';
-  const widenSidebar = '.p-workspace--context-pane-expanded { grid-template-columns: 0px auto 100% !important; }';
-  const disableTeamHeader = '.p-classic_nav__team_header { display: none !important; }';
-  const disableChannelHeader = '.p-classic_nav__channel_header { display: none !important; }';
-  const disableBody = '.p-workspace__primary_view { display: none !important; }';
-  return disableChannelList + widenSidebar + disableTeamHeader + disableChannelHeader + disableBody;
-}
-function getOnlyChannelCss() {
-  const disableBody = '.p-workspace__primary_view { display: none !important; }';
-  const disableChannelHeader = '.p-classic_nav__channel_header { display: none !important; }';
-  const disableRightHeader = '.p-classic_nav__right_header { display: none !important; }';
-  const disableSidebar = '.p-workspace__secondary_view { display: none !important; }';
-  return disableBody + disableChannelHeader + disableRightHeader + disableSidebar;
-}
-function getOnlyBodyCss() {
-  const disableChannelList = '.p-workspace__sidebar { display: none !important; }';
-  const disableTeamHeader = '.p-classic_nav__team_header { display: none !important; }';
-  const widenBody = '.p-workspace--context-pane-collapsed { grid-template-columns: 0px auto !important; }';
-  const adjustHeight = '.p-workspace--classic-nav { grid-template-rows: min-content 60px auto !important; }';
-  const adjustLeftPadding = '.p-workspace--context-pane-expanded { grid-template-columns: 0px auto !important; }';
-  return disableChannelList + widenBody + adjustHeight + disableTeamHeader + adjustLeftPadding;
-}
 /* eslint-disable no-unused-vars */
 function opendev() {
-  const webviews = getWebviews();
+  const webviews = Library.getWebviews();
   const webview = webviews[1];
   webview.openDevTools();
 }
@@ -186,11 +151,11 @@ function jumpLink(index) {
   });
 
   const element = document.getElementById(`${index.toString()}-message-url`);
-  if (!isUrl(element.value)) return;
+  if (!Library.isUrl(element.value)) return;
 
   const maybeUrl = new URL(element.value);
-  if (validateUrl(maybeUrl)) {
-    const slackUrl = modifySlackUrl(maybeUrl);
+  if (Library.validateUrl(maybeUrl)) {
+    const slackUrl = Library.modifySlackUrl(maybeUrl);
     loadURL(webview, slackUrl);
     element.value = '';
   }
@@ -201,41 +166,18 @@ function add() {
   const channelId = defaultChannel;
   initializeDiv(style, width);
 
-  const webview = getWebviews()[getNumberOfWebviews() - 1];
+  const webview = Library.getWebviews()[Library.getNumberOfWebviews() - 1];
   webview.addEventListener('dom-ready', () => {
     initializeWebview(webview, channelId);
   });
 }
 /* eslint-enable no-unused-vars */
-function isUrl(str) {
-  try {
-    const url = new URL(str);
-    return url;
-  } catch (err) {
-    return false;
-  }
-}
-function modifySlackUrl(url) {
-  const isThread = url.href.includes('thread_ts');
-  if (!isThread) {
-    return url.href.replace('archives', 'messages');
-  }
-
-  const parentMessageId = `p${url.search.replace(/(\?thread_ts=)|(&cid.*)|(\.)/g, '')}`;
-  const channelId = url.pathname.split('/')[2];
-  return `${url.origin}/messages/${channelId}/${parentMessageId}`;
-}
-function validateUrl(url) {
-  const cond1 = url.protocol === 'http:' || url.protocol === 'https:';
-  const cond2 = url.hostname.endsWith('.slack.com');
-  return cond1 && cond2;
-}
 function loadWorkspace(workspaceUrl) {
   const style = 'full-view';
   const width = 'large-tab';
   initializeDiv(style, width);
 
-  const webview = getWebviews()[getNumberOfWebviews() - 1];
+  const webview = Library.getWebviews()[Library.getNumberOfWebviews() - 1];
   webview.addEventListener('dom-ready', () => {
     initializeWebviewForAnotherWorkspace(webview, workspaceUrl);
   });
@@ -259,10 +201,10 @@ function initializeDiv(style, width) {
   incrementUniqueIndex();
 }
 function generateTab(width, style) {
-  const divContainer = createContainerDiv(getUniqueIndex(), width);
-  const divTabToolBar = createToolBarDiv();
-  const divWebview = createWebviewDiv();
-  const webview = createWebview(style);
+  const divContainer = Library.createContainerDiv(getUniqueIndex(), width);
+  const divTabToolBar = Library.createToolBarDiv();
+  const divWebview = Library.createWebviewDiv();
+  const webview = Library.createWebview(style);
   const root = getRootElement();
 
   root.appendChild(divContainer);
@@ -279,46 +221,10 @@ function generateTab(width, style) {
 function getRootElement() {
   return document.getElementsByClassName('horizontal-list')[0];
 }
-function createContainerDiv(index, width) {
-  const div = document.createElement('div');
-  div.id = index;
-  div.className = width;
-  return div;
-}
-function createToolBarDiv() {
-  const divTabToolBar = document.createElement('div');
-  divTabToolBar.className = 'tab-tool-bar';
-
-  const buttonDiv = document.createElement('div');
-  buttonDiv.className = 'tab-tool-bar-button';
-  divTabToolBar.appendChild(buttonDiv);
-
-  const channelNameDiv = document.createElement('div');
-  channelNameDiv.className = 'tab-tool-bar-channel';
-  divTabToolBar.appendChild(channelNameDiv);
-
-  return divTabToolBar;
-}
-function createWebviewDiv() {
-  const divWebview = document.createElement('div');
-  divWebview.className = 'webview';
-  return divWebview;
-}
-function createWebview(style) {
-  const webview = document.createElement('webview');
-  webview.src = 'about:blank';
-  webview.id = style;
-  return webview;
-}
-function setWebviewAutosize(webview, autosize) {
-  const hoge = webview;
-  hoge.autosize = autosize;
-  return hoge;
-}
 function selectAplicableCss(webview, { onlyBodyCss, onlyChannelCss, onlySidebarCss }) {
-  if (shouldRenderOnlyBody(webview)) { applyCss(webview, onlyBodyCss); }
-  if (shouldRenderOnlyChannelList(webview)) { applyCss(webview, onlyChannelCss); }
-  if (shouldRenderOnlySidebar(webview)) { applyCss(webview, onlySidebarCss); }
+  if (Library.shouldRenderOnlyBody(webview)) { applyCss(webview, onlyBodyCss); }
+  if (Library.shouldRenderOnlyChannelList(webview)) { applyCss(webview, onlyChannelCss); }
+  if (Library.shouldRenderOnlySidebar(webview)) { applyCss(webview, onlySidebarCss); }
 }
 function registerToOpenUrl(webview) {
   // Hack: remove EventListener if already added
@@ -332,22 +238,6 @@ function openExternalUrl(event) {
   if (url.startsWith('http://') || url.startsWith('https://')) {
     shell.openExternal(url);
   }
-}
-function getChannelUrl(baseUrl, channelId) {
-  const url = `messages/${channelId}`;
-  return new URL(url, baseUrl).href.toString();
-}
-function shouldRenderOnlyChannelList(webview) {
-  return webview.id === 'channel-only';
-}
-function shouldRenderOnlyBody(webview) {
-  return webview.id === 'body-only';
-}
-function shouldRenderOnlySidebar(webview) {
-  return webview.id === 'sidebar-only';
-}
-function checkUrlIsDefault(webview) {
-  return webview.attributes.src.value === 'about:blank';
 }
 function loadURL(webview, url) {
   webview.loadURL(url.toString());
@@ -376,26 +266,10 @@ function openFileAndSave() {
 }
 function saveJson(jsonPath) {
   const settings = JSON.parse(fs.readFileSync(jsonPath));
-  if (!validateJson(settings)) { return; }
+  if (!Library.validateJson(settings)) { return; }
 
   store.set(settings);
   forceReload();
-}
-function validateJson(jsonObj) {
-  if (!jsonObj.url) {
-    alert('jsonObj.url is invalid');
-    return false;
-  }
-  if (!jsonObj.other_urls) {
-    alert('jsonObj.other_urls is invalid');
-    return false;
-  }
-  if (!jsonObj.contents) {
-    alert('jsonObj.contents is invalid');
-    return false;
-  }
-
-  return true;
 }
 function forceReload() {
   remote.getCurrentWindow().reload();
@@ -418,7 +292,7 @@ function buildJsonObjectFromStoredData() {
     other_urls: store.get('other_urls'),
     contents: store.get('contents'),
   };
-  if (!validateJson(jsonObj)) { return null; }
+  if (!Library.validateJson(jsonObj)) { return null; }
 
   return jsonObj;
 }
---
+++ test/lib/lib.test.js
@@ -0,0 +1,195 @@
+const Library = require('../../src/lib/lib');
+
+describe('getOtherWorkspacesInfo', () => {
+  test('passes', () => {
+    const otherUrls = ['https://sample.slack.com/'];
+    const expected = [{ name: 'sample', url: new URL('https://sample.slack.com/') }];
+    expect(Library.getOtherWorkspacesInfo(otherUrls)).toMatchObject(expected);
+  });
+});
+
+describe('getWebviews', () => {
+  test('passes', () => {
+    document.body.innerHTML = '<webview></webview>';
+    expect(Library.getWebviews().length).toBe(1);
+  });
+});
+
+describe('getOnly*Css', () => {
+  test('passes', () => {
+    expect(typeof Library.getOnlySidebarCss()).toBe('string');
+    expect(typeof Library.getOnlyChannelCss()).toBe('string');
+    expect(typeof Library.getOnlyBodyCss()).toBe('string');
+  });
+});
+
+describe('isUrl', () => {
+  test('is url', () => {
+    const urlString = 'https://sample.slack.com/';
+    expect(Library.isUrl(urlString)).toMatchObject(new URL(urlString));
+  });
+
+  test('is not url', () => {
+    expect(Library.isUrl('foobar')).toBe(false);
+  });
+});
+
+describe('modifySlackUrl', () => {
+  test('is Thread', () => {
+    const url = new URL('https://sample.slack.com/archives/ABC/pxxx?thread_ts=123.45&cid=DEF');
+    const expected = 'https://sample.slack.com/messages/ABC/p12345';
+
+    expect(typeof Library.modifySlackUrl(url)).toBe('string');
+    expect(Library.modifySlackUrl(url)).toBe(expected);
+  });
+
+  test('is not Thread', () => {
+    const url = new URL('https://sample.slack.com/archives/ABC/pxxx');
+    const expected = 'https://sample.slack.com/messages/ABC/pxxx';
+
+    expect(typeof Library.modifySlackUrl(url)).toBe('string');
+    expect(Library.modifySlackUrl(url)).toBe(expected);
+  });
+});
+
+describe('validateUrl', () => {
+  test('is valid', () => {
+    const url1 = new URL('https://sample.slack.com');
+    const url2 = new URL('http://sample.slack.com');
+    expect(Library.validateUrl(url1)).toBe(true);
+    expect(Library.validateUrl(url2)).toBe(true);
+  });
+
+  test('is not valid', () => {
+    const url = new URL('http://sample.com');
+    expect(Library.validateUrl(url)).toBe(false);
+  });
+});
+
+describe('createContainerDiv', () => {
+  test('create div', () => {
+    const div = Library.createContainerDiv(1, 'large');
+    expect(div.id).toBe('1');
+    expect(div.className).toBe('large');
+  });
+});
+
+describe('createToolBarDiv', () => {
+  test('create div', () => {
+    const div = Library.createToolBarDiv();
+    expect(div.childNodes.length).toBe(2);
+    expect(div.className).toBe('tab-tool-bar');
+    expect(div.firstChild.className).toBe('tab-tool-bar-button');
+    expect(div.lastChild.className).toBe('tab-tool-bar-channel');
+  });
+});
+
+describe('createWebviewDiv', () => {
+  test('create div', () => {
+    const div = Library.createWebviewDiv();
+    expect(div.className).toBe('webview');
+  });
+});
+
+describe('createWebview', () => {
+  test('create webview', () => {
+    const webview = Library.createWebview('body-only');
+    expect(webview.tagName).toBe('WEBVIEW');
+    expect(webview.src).toBe('about:blank');
+    expect(webview.id).toBe('body-only');
+  });
+});
+
+describe('setWebviewAutosize', () => {
+  test('returns autosizable webview', () => {
+    const webview = document.createElement('webview');
+    const newWebview = Library.setWebviewAutosize(webview, true);
+    expect(newWebview.tagName).toBe('WEBVIEW');
+    expect(webview.autosize).toBe(true);
+  });
+
+  test('returns no autosizable webview', () => {
+    const webview = document.createElement('webview');
+    const newWebview = Library.setWebviewAutosize(webview, false);
+    expect(newWebview.tagName).toBe('WEBVIEW');
+    expect(webview.autosize).toBe(false);
+  });
+});
+
+describe('getChannelUrl', () => {
+  test('returns url', () => {
+    const baseUrl = 'https://sample.slack.com';
+    const urlString = Library.getChannelUrl(baseUrl, 'AAA');
+    expect(urlString).toBe('https://sample.slack.com/messages/AAA');
+  });
+});
+
+describe('shouldRenderOnly*', () => {
+  test('channel list', () => {
+    const webview = document.createElement('webview');
+    webview.id = 'channel-only';
+    expect(Library.shouldRenderOnlyChannelList(webview)).toBe(true);
+  });
+
+  test('side bar', () => {
+    const webview = document.createElement('webview');
+    webview.id = 'sidebar-only';
+    expect(Library.shouldRenderOnlySidebar(webview)).toBe(true);
+  });
+
+  test('body', () => {
+    const webview = document.createElement('webview');
+    webview.id = 'body-only';
+    expect(Library.shouldRenderOnlyBody(webview)).toBe(true);
+  });
+
+  test('no match', () => {
+    const webview = document.createElement('webview');
+    webview.id = 'foo-only';
+
+    expect(Library.shouldRenderOnlyChannelList(webview)).toBe(false);
+    expect(Library.shouldRenderOnlySidebar(webview)).toBe(false);
+    expect(Library.shouldRenderOnlyBody(webview)).toBe(false);
+  });
+});
+
+// describe('checkUrlIsDefault', () => {
+//   test('is default', () => {
+//     const webview = document.createElement('webview');
+//     // webview.attributes.src.value = 'about:blank';
+//     expect(Library.checkUrlIsDefault(webview)).toBe(true);
+//   });
+//
+//   test('is not default', () => {
+//     const webview = document.createElement('webview');
+//     // webview.attributes.src.value = ''
+//     expect(Library.checkUrlIsDefault(webview)).toBe(false);
+//   });
+// });
+
+describe('validateJson', () => {
+  test('valid', () => {
+    const json = { url: 'url', other_urls: 'other_urls', contents: 'contents' };
+    expect(Library.validateJson(json)).toBe(true);
+  });
+
+  /* eslint-disable no-undef */
+  test('no url', () => {
+    const json = { other_urls: 'other_urls', contents: 'contents' };
+    window.alert = jest.fn();
+    expect(Library.validateJson(json)).toBe(false);
+  });
+
+  test('no other_urls', () => {
+    const json = { url: 'url', contents: 'contents' };
+    window.alert = jest.fn();
+    expect(Library.validateJson(json)).toBe(false);
+  });
+
+  test('no contents', () => {
+    const json = { url: 'url', other_urls: 'other_urls' };
+    window.alert = jest.fn();
+    expect(Library.validateJson(json)).toBe(false);
+  });
+  /* eslint-enable no-undef */
+});
